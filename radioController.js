61110c00612eeebfff15a46a0ebc6c3223389005
Rui Fartaria <fc18752@alunos.fc.ul.pt>
2019-04-27 19:52:20 +0100

    inicio radio

---
+++ radioController.js
@@ -0,0 +1,9 @@
+// controlador do ecrã HELP1
+
+var infoVeiculoController = {
+    id: "RADIO",
+    url: "radioView.html",
+    css: "radioStyles.css",
+    iconHtml: `<div class="hud-icon" id="icon-ecra-RADIO">RADIO</div>`
+}
+
