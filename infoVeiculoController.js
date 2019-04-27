9f6ae7ac968c81e697db0d88369e2e013847b155
Rui Fartaria <fc18752@alunos.fc.ul.pt>
2019-04-27 19:52:01 +0100

    inicio info veículo

---
+++ infoVeiculoController.js
@@ -0,0 +1,9 @@
+// controlador do ecrã HELP1
+
+var infoVeiculoController = {
+    id: "INFOVEICULO",
+    url: "infoVeiculoView.html",
+    css: "infoVeiculoStyles.css",
+    iconHtml: `<div class="hud-icon" id="icon-ecra-INFOVEICULO">INFO_V</div>`
+}
+
