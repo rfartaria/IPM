c6241d5e67c795072d011f95e1ddebe96c4d92c8
Rui Fartaria <fc18752@alunos.fc.ul.pt>
2019-04-27 19:51:43 +0100

    inicio chamadas

---
+++ chamadasController.js
@@ -0,0 +1,9 @@
+// controlador do ecrã HELP1
+
+var infoVeiculoController = {
+    id: "CHAMADAS",
+    url: "chamadasView.html",
+    css: "chamadasStyles.css",
+    iconHtml: `<div class="hud-icon" id="icon-ecra-CHAMADAS">CHAMADAS</div>`
+}
+
