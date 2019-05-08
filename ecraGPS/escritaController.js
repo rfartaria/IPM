589712579f81a8f08f55e2bd422eeaca78140a2a
Tiagofvarela <Tiagofvarela@users.noreply.github.com>
2019-05-08 13:55:30 +0100

    Implementado ecrã intermediário entre escrita de GPS e o HUD, semelhante ao do rádio.

---
+++ ecraGPS/escritaController.js
@@ -0,0 +1,231 @@
+// controlador do ecrã GPS
+
+var escritaController = {
+    id: "ESCRITA",
+    url: "ecraGPS/escritaView.html",
+    css: "ecraGPS/escritaStyles.css",
+    iconHtml: `<div class="hud-icon" id="icon-ecra-GPS"><i class="fas fa-globe-europe" style="font-size: 36px; line-height: 70px;"></i></div>`,
+
+    listaLetras: ["<i class='fas fa-check' style='font-size: 14pt;'></i>","_","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
+    indiceInicial: 0,
+    indiceFim: 6,
+    numElem: 7,
+
+    listaSugestoes: ["RUA\xa0DAS\xa0FLORES","RUA\xa0DOS\xa0DIAMANTES","AVENIDA\xa0AVENIDO","LARGO\xa0DOS\xa0PATOS","RUA\xa0FELIZ","AVENIDA\xa0DA\xa0IGUALDADE"],
+    nSugestoes : 0,
+
+    sugestaoSelecionada: 0,
+
+    setAccoesIniciais: function() {
+        //SCROLL
+        HUD.accoes.scroll = function(e) {
+            var scrollDirectionUP = e.deltaY < 0;
+            if (HUD.ecraActual) {
+                if (scrollDirectionUP) {
+                    escritaController.previousLetra();
+                } else {
+                    escritaController.nextLetra();
+                }
+            }
+        }
+        //VOLTAR
+        HUD.accoes.clickBack = function() {
+            //Se a barra estiver vazia, sair do ecra
+            if($(".barra_de_texto").text() == "|"){
+                var aRemover = $('#alfabeto li:eq(0)');
+                aRemover.removeClass("opcao-seleccionada");
+            // $('#opcoes-radio li:eq('+(radioController.opcaoActual)+')').removeClass('RADIO-opcao-seleccionada');
+            /*radioController.opcaoActual = undefined;
+            radioController.updateInterface();*/
+                /*HUD.setEcraInactivo();
+                HUD.setAccoesPadrao();*/
+                gpsController.loadOwnEcraView();
+                gpsController.setAccoesIniciais();
+            }else{
+                escritaController.apagaLetra();
+                $(".barra_de_texto").append("|");
+
+                escritaController.atualizaSugestao();
+            }
+        }
+        //OK
+        HUD.accoes.clickOK = function() {
+            var letra = escritaController.listaLetras[escritaController.indiceInicial];
+
+            if(escritaController.listaLetras[escritaController.indiceInicial] == "<i class='fas fa-check' style='font-size: 14pt;'></i>"){
+                    if(escritaController.nSugestoes > 0){
+                        escritaController.sugestaoSelecionada = 0;
+                        var aRemover = $('#alfabeto li:eq(0)');
+                        aRemover.removeClass("opcao-seleccionada");
+                        var aSelecionar = $('#lista-sugestoes li:eq('+escritaController.sugestaoSelecionada+')');
+                        aSelecionar.addClass("opcao-seleccionada");
+                        escritaController.setAccoesSugestoes();
+                    }
+            }else{
+                escritaController.adicionaLetra(letra);
+                escritaController.atualizaSugestao();
+            }
+        }
+    },
+
+    atualizaSugestao: function(){
+        escritaController.limpaSugestoes();
+        var texto = $(".barra_de_texto").text();
+        texto = texto.substring(0, texto.length - 1);
+        if(texto !=""){
+            for(var i = 0; i < escritaController.listaSugestoes.length && escritaController.nSugestoes < 4; i++){
+                if(escritaController.listaSugestoes[i].startsWith(texto)){
+                    document.getElementById("sugestao"+(escritaController.nSugestoes+1)).innerHTML = 
+                    escritaController.listaSugestoes[i];
+                    escritaController.nSugestoes++;
+                }  
+            }
+            for(var i = 0; i < escritaController.listaSugestoes.length && escritaController.nSugestoes < 4; i++){
+                if(!escritaController.listaSugestoes[i].startsWith(texto) && escritaController.listaSugestoes[i].includes(texto)){
+                    document.getElementById("sugestao"+(escritaController.nSugestoes+1)).innerHTML = 
+                    escritaController.listaSugestoes[i];
+                    escritaController.nSugestoes++;
+                }
+            }
+        }
+    },
+
+    limpaSugestoes: function(){
+        for(var i = 0; i < 5; i++){
+            var aRetirar = $('#lista-sugestoes li:eq('+i+')');
+            aRetirar.text("");
+        }
+        escritaController.nSugestoes = 0;
+    },
+
+    adicionaLetra: function(l){
+        //apagar o | 
+        escritaController.apagaChar();
+        if(l == "_"){
+            $(".barra_de_texto").append("\xa0|");
+        }else{
+            //vai buscar class barra e adiciona ao fim a letra selecionada
+            $(".barra_de_texto").append(l + "|");
+        }
+    },
+
+    //apenas apaga o ultimo char na barra de texto.
+    apagaChar: function(){
+        var texto = $(".barra_de_texto").text();
+        texto = texto.substring(0, texto.length - 1);
+        $(".barra_de_texto").text(texto);
+    },
+
+    //apaga o "|" e a letra apenas. Dever ser escrito algo a seguir de imediato
+    apagaLetra: function(){
+        escritaController.apagaChar();
+        escritaController.apagaChar();
+    },
+
+    previousLetra: function(){
+        if(escritaController.indiceInicial == 0){
+            escritaController.indiceInicial = 27;
+        }else{
+            escritaController.indiceInicial--;
+        }
+        if(escritaController.indiceFim == 0){
+            escritaController.indiceFim = 27;
+        }else{
+            escritaController.indiceFim--;
+        }
+        for(var i = 0; i < escritaController.numElem; i++){
+            var aux = i + 1;
+            document.getElementById("letra"+aux).innerHTML = escritaController.listaLetras[(escritaController.indiceInicial+i)%28];
+        }
+    },
+
+    nextLetra: function(){
+        if(escritaController.indiceInicial == 27){
+            escritaController.indiceInicial = 0;
+        }else{
+            escritaController.indiceInicial++;
+        }
+        if(escritaController.indiceFim == 27){
+            escritaController.indiceFim = 0;
+        }else{
+            escritaController.indiceFim++;
+        }
+        for(var i = 0; i < escritaController.numElem; i++){
+            var aux = i + 1;
+            document.getElementById("letra"+aux).innerHTML = escritaController.listaLetras[(escritaController.indiceInicial+i)%28];
+            //$("#letra"+aux).text(escritaController.listaLetras[escritaController.indiceInicial+i]);
+        }
+    },
+    
+    setAccoesSugestoes: function(){
+        HUD.accoes.scroll = function(e) {
+            var scrollDirectionUP = e.deltaY < 0;
+            if (HUD.ecraActual) {
+                if (scrollDirectionUP) {
+                    escritaController.previousOpcao();
+                } else {
+                    escritaController.nextOpcao();
+                }
+            }
+        }
+        //VOLTAR
+        HUD.accoes.clickBack = function() {
+            var aRemover = $('#lista-sugestoes li:eq('+(escritaController.sugestaoSelecionada)+')');
+            aRemover.removeClass('opcao-seleccionada');
+            var aSelecionar = $('#alfabeto li:eq(0)');
+            aSelecionar.addClass("opcao-seleccionada");
+            escritaController.setAccoesIniciais();
+        }
+        //OK
+        HUD.accoes.clickOK = function() {
+            var aTirar = $('#lista-sugestoes li:eq('+(escritaController.sugestaoSelecionada)+')');
+            aTirar.removeClass('opcao-seleccionada');
+
+            gpsController.loadOwnEcraView();
+            gpsController.setAccoesIniciais();
+            gpsController.destinoAtual = aTirar.text();
+
+            /*HUD.setEcraInactivo();
+            HUD.setAccoesPadrao();*/
+            
+        }
+    },
+
+    previousOpcao: function(){
+        if(escritaController.sugestaoSelecionada != 0){
+            escritaController.sugestaoSelecionada--;
+            var aRemover = $('#lista-sugestoes li:eq('+(escritaController.sugestaoSelecionada+1)+')');
+            aRemover.removeClass('opcao-seleccionada');
+            var aSelecionar = $('#lista-sugestoes li:eq('+escritaController.sugestaoSelecionada+')');
+            aSelecionar.addClass('opcao-seleccionada');
+        }
+    },
+
+    nextOpcao: function(){
+        if(escritaController.sugestaoSelecionada != escritaController.nSugestoes-1){
+            escritaController.sugestaoSelecionada++;
+            var aRemover = $('#lista-sugestoes li:eq('+(escritaController.sugestaoSelecionada-1)+')');
+            aRemover.removeClass('opcao-seleccionada');
+            var aSelecionar = $('#lista-sugestoes li:eq('+escritaController.sugestaoSelecionada+')');
+            aSelecionar.addClass('opcao-seleccionada');
+        }
+    },
+
+    //Carrega o HTML do proprio ecra
+    loadOwnEcraView: function() {
+        //$("#hud-screen-container").load(opcoesControllers[opcaoActual].url);
+        if (HUD.ecraActual) {
+            $.get(escritaController.url, function(data) {
+                $("#hud-screen-container").html(data);
+                var aSelecionar = $('#alfabeto li:eq(0)');
+                aSelecionar.addClass("opcao-seleccionada");
+            })
+            .fail(function(){
+                alert("não consegui obter html da view!");
+            });
+        } else {
+            $("#hud-screen-container").html('');
+        }
+    },
+}
+
--- ecraGPS/gpsController.js
+++ ecraGPS/gpsController.js
@@ -1,4 +1,4 @@
-// controlador do ecrã GPS
+// controlador do ecrã de GPS
 
 var gpsController = {
     id: "GPS",
@@ -6,158 +6,34 @@ var gpsController = {
     css: "ecraGPS/gpsStyles.css",
     iconHtml: `<div class="hud-icon" id="icon-ecra-GPS"><i class="fas fa-globe-europe" style="font-size: 36px; line-height: 70px;"></i></div>`,
 
-    listaLetras: ["<i class='fas fa-check' style='font-size: 14pt;'></i>","_","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
-    indiceInicial: 0,
-    indiceFim: 6,
-    numElem: 7,
+    destinoAtual: undefined,
+    opcaoActual: undefined,
+    opcoesControllers: [],
 
-    listaSugestoes: ["RUA\xa0DAS\xa0FLORES","RUA\xa0DOS\xa0DIAMANTES","AVENIDA\xa0AVENIDO","LARGO\xa0DOS\xa0PATOS","RUA\xa0FELIZ","AVENIDA\xa0DA\xa0IGUALDADE"],
-    nSugestoes : 0,
+    //Coloca as variaveis iniciais (o acima devia faze-lo?)
+    setUp: function(){
+        //Tal como em core, uma lista de dois elementos que guarda os controladores de frequencia e estacao
+        gpsController.opcoesControllers = [ undefined, escritaController ];
+        gpsController.opcaoActual = undefined;
+        //gpsController.destinoAtual = "";
+    },
 
-    sugestaoSelecionada: 0,
-
+    //Regista as accoes iniciais
     setAccoesIniciais: function() {
-        var aSelecionar = $('#alfabeto li:eq(0)');
-        aSelecionar.addClass("opcao-seleccionada");
-        //SCROLL
-        HUD.accoes.scroll = function(e) {
-            var scrollDirectionUP = e.deltaY < 0;
-            if (HUD.ecraActual) {
-                if (scrollDirectionUP) {
-                    gpsController.previousLetra();
-                } else {
-                    gpsController.nextLetra();
-                }
-            }
-        }
-        //VOLTAR
-        HUD.accoes.clickBack = function() {
-            //Se a barra estiver vazia, sair do ecra
-            if($(".barra_de_texto").text() == "|"){
-                var aRemover = $('#alfabeto li:eq(0)');
-                aRemover.removeClass("opcao-seleccionada");
-            // $('#opcoes-radio li:eq('+(radioController.opcaoActual)+')').removeClass('RADIO-opcao-seleccionada');
-            /*radioController.opcaoActual = undefined;
-            radioController.updateInterface();*/
-                HUD.setEcraInactivo();
-                HUD.setAccoesPadrao();
-            }else{
-                gpsController.apagaLetra();
-                $(".barra_de_texto").append("|");
-
-                gpsController.atualizaSugestao();
-            }
-        }
-        //OK
+        // gpsController.setUp();
         HUD.accoes.clickOK = function() {
-            var letra = gpsController.listaLetras[gpsController.indiceInicial];
-
-            if(gpsController.listaLetras[gpsController.indiceInicial] == "<i class='fas fa-check' style='font-size: 14pt;'></i>" &&
-                nSugestoes > 0){
-                var aRemover = $('#alfabeto li:eq(0)');
-                aRemover.removeClass("opcao-seleccionada");
-                var aSelecionar = $('#lista-sugestoes li:eq('+gpsController.sugestaoSelecionada+')');
-                aSelecionar.addClass("opcao-seleccionada");
-                gpsController.setAccoesSugestoes();
-            }else{
-                gpsController.adicionaLetra(letra);
-                gpsController.atualizaSugestao();
+            if(gpsController.opcaoActual == 2){
+                gpsController.destinoAtual = "Nada";
+                gpsController.updateInterface();
+            }
+            if (HUD.ecraActual) {
+                gpsController.opcoesControllers[gpsController.opcaoActual].loadOwnEcraView();
+                //alert("espera ai");
+                gpsController.opcoesControllers[gpsController.opcaoActual].setAccoesIniciais();
+                //o ecra de frequencias nao serah implementado
             }
         }
-    },
 
-    atualizaSugestao: function(){
-        gpsController.limpaSugestoes();
-        var texto = $(".barra_de_texto").text();
-        texto = texto.substring(0, texto.length - 1);
-        if(texto !=""){
-            for(var i = 0; i < gpsController.listaSugestoes.length && gpsController.nSugestoes < 4; i++){
-                if(gpsController.listaSugestoes[i].startsWith(texto)){
-                    document.getElementById("sugestao"+(gpsController.nSugestoes+1)).innerHTML = 
-                    gpsController.listaSugestoes[i];
-                    gpsController.nSugestoes++;
-                }  
-            }
-            for(var i = 0; i < gpsController.listaSugestoes.length && gpsController.nSugestoes < 4; i++){
-                if(!gpsController.listaSugestoes[i].startsWith(texto) && gpsController.listaSugestoes[i].includes(texto)){
-                    document.getElementById("sugestao"+(gpsController.nSugestoes+1)).innerHTML = 
-                    gpsController.listaSugestoes[i];
-                    gpsController.nSugestoes++;
-                }
-            }
-        }
-    },
-
-    limpaSugestoes: function(){
-        for(var i = 0; i < 5; i++){
-            var aRetirar = $('#lista-sugestoes li:eq('+i+')');
-            aRetirar.text("");
-        }
-        gpsController.nSugestoes = 0;
-    },
-
-    adicionaLetra: function(l){
-        //apagar o | 
-        gpsController.apagaChar();
-        if(l == "_"){
-            $(".barra_de_texto").append("\xa0|");
-        }else{
-            //vai buscar class barra e adiciona ao fim a letra selecionada
-            $(".barra_de_texto").append(l + "|");
-        }
-    },
-
-    //apenas apaga o ultimo char na barra de texto.
-    apagaChar: function(){
-        var texto = $(".barra_de_texto").text();
-        texto = texto.substring(0, texto.length - 1);
-        $(".barra_de_texto").text(texto);
-    },
-
-    //apaga o "|" e a letra apenas. Dever ser escrito algo a seguir de imediato
-    apagaLetra: function(){
-        gpsController.apagaChar();
-        gpsController.apagaChar();
-    },
-
-    previousLetra: function(){
-        if(gpsController.indiceInicial == 0){
-            gpsController.indiceInicial = 27;
-        }else{
-            gpsController.indiceInicial--;
-        }
-        if(gpsController.indiceFim == 0){
-            gpsController.indiceFim = 27;
-        }else{
-            gpsController.indiceFim--;
-        }
-        for(var i = 0; i < gpsController.numElem; i++){
-            var aux = i + 1;
-            document.getElementById("letra"+aux).innerHTML = gpsController.listaLetras[(gpsController.indiceInicial+i)%28];
-        }
-    },
-
-    nextLetra: function(){
-        if(gpsController.indiceInicial == 27){
-            gpsController.indiceInicial = 0;
-        }else{
-            gpsController.indiceInicial++;
-        }
-        if(gpsController.indiceFim == 27){
-            gpsController.indiceFim = 0;
-        }else{
-            gpsController.indiceFim++;
-        }
-        for(var i = 0; i < gpsController.numElem; i++){
-            var aux = i + 1;
-            document.getElementById("letra"+aux).innerHTML = gpsController.listaLetras[(gpsController.indiceInicial+i)%28];
-            //$("#letra"+aux).text(gpsController.listaLetras[gpsController.indiceInicial+i]);
-        }
-    },
-    
-    setAccoesSugestoes: function(){
-        var aSelecionar = $('#lista-sugestoes li:eq('+gpsController.sugestaoSelecionada+')');
-        aSelecionar.addClass('opcao-seleccionada');
         HUD.accoes.scroll = function(e) {
             var scrollDirectionUP = e.deltaY < 0;
             if (HUD.ecraActual) {
@@ -168,41 +44,65 @@ var gpsController = {
                 }
             }
         }
-        //VOLTAR
-        HUD.accoes.clickBack = function() {
-            var aRemover = $('#lista-sugestoes li:eq('+(gpsController.sugestaoSelecionada)+')');
-            aRemover.removeClass('opcao-seleccionada');
-            gpsController.setAccoesIniciais();
-        }
-        //OK
-        HUD.accoes.clickOK = function() {
-            var aTirar = $('#lista-sugestoes li:eq('+(gpsController.sugestaoSelecionada)+')');
-            aTirar.removeClass('opcao-seleccionada');
 
+        HUD.accoes.clickBack = function() {
+            // $('#opcoes-radio li:eq('+(gpsController.opcaoActual)+')').removeClass('opcao-seleccionada');
+            gpsController.opcaoActual = undefined;
+            gpsController.updateInterface();
             HUD.setEcraInactivo();
             HUD.setAccoesPadrao();
-            
+        }
+    },
+    
+    //Carrega o HTML do proprio ecra
+    loadOwnEcraView: function() {
+        //$("#hud-screen-container").load(opcoesControllers[opcaoActual].url);
+        if (HUD.ecraActual) {
+            $.get(/*gpsController.opcoesControllers[gpsController.opcaoActual]*/gpsController.url, function(data) {
+                $("#hud-screen-container").html(data);
+                //inicializar variaveis eh feito aqui
+                /*gpsController.opcoesControllers = [ escritaController, undefined ];
+                gpsController.opcaoActual = 0;*/
+                gpsController.updateInterface();
+            })
+            .fail(function(){
+                alert("não consegui obter html da view!");
+            });
+        } else {
+            $("#hud-screen-container").html('');
         }
     },
 
-    previousOpcao: function(){
-        if(gpsController.sugestaoSelecionada != 0){
-            gpsController.sugestaoSelecionada--;
-            var aRemover = $('#lista-sugestoes li:eq('+(gpsController.sugestaoSelecionada+1)+')');
-            aRemover.removeClass('opcao-seleccionada');
-            var aSelecionar = $('#lista-sugestoes li:eq('+gpsController.sugestaoSelecionada+')');
-            aSelecionar.addClass('opcao-seleccionada');
+    //Atualiza as selecoes no ecra
+    updateInterface: function() {
+        var aTirar = $('#opcoes-gps li:eq(0)');
+        aTirar.removeClass('opcao-seleccionada');
+        aTirar = $('#opcoes-gps li:eq(1)');
+        aTirar.removeClass('opcao-seleccionada');
+        aTirar = $('#opcoes-gps li:eq(2)');
+        aTirar.removeClass('opcao-seleccionada');
+        
+        var aSelecionar = $('#opcoes-gps li:eq('+(gpsController.opcaoActual)+')');
+        aSelecionar.addClass('opcao-seleccionada');
+
+        $('#GPS-destino-actual').text(gpsController.destinoAtual);
+    },
+
+    //FUNCOES
+    //nestes casos, a lista nunca mudaria no numero de opcoes, mesmo na implementacao completa -TV
+    previousOpcao: function() {
+        if(gpsController.opcaoActual >= 1){
+            gpsController.opcaoActual--;
+            gpsController.updateInterface();
         }
     },
 
-    nextOpcao: function(){
-        if(gpsController.sugestaoSelecionada != gpsController.nSugestoes-1){
-            gpsController.sugestaoSelecionada++;
-            var aRemover = $('#lista-sugestoes li:eq('+(gpsController.sugestaoSelecionada-1)+')');
-            aRemover.removeClass('opcao-seleccionada');
-            var aSelecionar = $('#lista-sugestoes li:eq('+gpsController.sugestaoSelecionada+')');
-            aSelecionar.addClass('opcao-seleccionada');
+    nextOpcao: function() {
+        if(gpsController.opcaoActual <= 1){
+            gpsController.opcaoActual++;
+            gpsController.updateInterface();
         }
     }
+
 }
 
