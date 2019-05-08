e44cf823839477654ac64fe4ce633ef44559e87b
Tiagofvarela <Tiagofvarela@users.noreply.github.com>
2019-05-08 11:06:32 +0100

    Para a primeira letra, surge lista de sugestões.

--- ecraGPS/gpsController.js
+++ ecraGPS/gpsController.js
@@ -54,10 +54,17 @@ var gpsController = {
     },
 
     atualizaSugestao: function(){
-        var texto = $(".barra_de_texto").text()
+        var texto = $(".barra_de_texto").text();
         texto = texto.substring(0, texto.length - 1);
         for(var i = 0; i < gpsController.listaSugestoes.length && gpsController.nSugestoes < 4; i++){
-            if(gpsController.listaSugestoes[i].includes(texto)){
+            if(gpsController.listaSugestoes[i].startsWith(texto)){
+                document.getElementById("sugestao"+(gpsController.nSugestoes+1)).innerHTML = 
+                gpsController.listaSugestoes[i];
+                gpsController.nSugestoes++;
+            }  
+        }
+        for(var i = 0; i < gpsController.listaSugestoes.length && gpsController.nSugestoes < 4; i++){
+            if(!gpsController.listaSugestoes[i].startsWith(texto) && gpsController.listaSugestoes[i].includes(texto)){
                 document.getElementById("sugestao"+(gpsController.nSugestoes+1)).innerHTML = 
                 gpsController.listaSugestoes[i];
                 gpsController.nSugestoes++;
